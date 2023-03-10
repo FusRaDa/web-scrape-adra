684eecd169c4446887e316bbd2c66689aabd4987
Matthew Rada <matthewr7.christopher@gmail.com>
2023-03-10 08:46:20 -0500

    add timer to avoid 429

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 from bs4 import BeautifulSoup
 import requests
 import configparser
@@ -6,8 +8,7 @@ parser = configparser.ConfigParser()
 parser.read('config.txt')
 class_category = parser.get('config', 'organization_type')
 is_active = parser.get('config', 'is_active')
-
-
+name = parser.get('config', 'search_term')
 
 
 def get_class_from_category(string):
@@ -35,30 +36,54 @@ def get_class_from_category(string):
         return 0
 
 
+form_data = {
+    'name': name,
+    'class': get_class_from_category(class_category),
+    'authenticity_token': 'DAXOl8nWaQv5NLV+OFRWVreESfiM4bbQm3B8rSMwaAc=',
+    'is_active': is_active,
+    'commit': 'Search',
+}
+
+
 def get_urls_list(html):
+    links = []
     soup = BeautifulSoup(html, 'lxml')
     url_list = soup.find_all('tr', class_='results-line-1')
     for url in url_list:
-        link = url.a['href']
-        print(link)
+        links.append(url.a['href'])
+    return links
 
 
-def gather_from_page():
+def scrape_data_from_url_page(html):
+    return "hi"
 
-    url = 'https://eadventist.net/en/search?page=1&type=a'
 
-    form_data = {
-        'class': get_class_from_category(class_category),
-        'authenticity_token': 'DAXOl8nWaQv5NLV+OFRWVreESfiM4bbQm3B8rSMwaAc=',
-        'is_active': is_active,
-        'commit': 'Search',
-    }
+def get_results_page(page):
+    string_number = page.__str__()
+    url = 'https://eadventist.net/en/search?page=' + string_number + '&type=a'
+
+    time.sleep(5)  # avoid 429 too many requests
 
     server = requests.post(url, data=form_data)
     search_results_html = server.text
 
-    get_urls_list(search_results_html)
+    status_code = server
+    print(status_code)
+
+    url_list = get_urls_list(search_results_html)
+
+    # for url in url_list:
+    #     # add scrape data method here
+    #     print(url)
+
+    return len(url_list)
 
 
-gather_from_page()
+def go_to_next_page():
+    page = 1
 
+    while get_results_page(page) > 0:
+        print('yay')
+        page += 1
+
+go_to_next_page()
